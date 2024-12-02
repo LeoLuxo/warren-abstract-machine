49b5bca8756ed31a0ae4c3b8717e4b820f2bfe3e
LeoLuxo <leo@gamboa.eu>
2024-12-02 15:03:23 +0100

    Cleanup utils

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ fn main() -> Result<()> {
 	// solve::<L0>("p(a)".parse_as()?, "p(X)".parse_as()?)?;
 	// solve::<L0>("p(X, Y, X)".parse_as()?, "p(X, Y, Z)".parse_as()?)?;
 	// solve::<L0>("p(Y, Y, Z)".parse_as()?, "p(X, Y, Z)".parse_as()?)?;
-	// solve::<L0>("p(X, Y, Y)".parse_as()?, "p(X, Y, Z)".parse_as()?)?;
+	solve::<L0>("p(X, Y, Y)".parse_as()?, "p(X, Y, Z)".parse_as()?)?;
 	// solve::<L0>("p(f(X), h(Y, f(a)), Y)".parse_as()?, "p(Z, h(Z,W), f(W))".parse_as()?)?;
-	solve::<L0>("p(f(X), h(Y, f(Z)), Y)".parse_as()?, "p(Z, h(Z,W), f(W))".parse_as()?)?;
+	// solve::<L0>("p(f(X), h(Y, f(Z)), Y)".parse_as()?, "p(Z, h(Z,W), f(W))".parse_as()?)?;
 
 	// println!("{}", "p(f(X), h(Y, f(a)), Y).".parse::<Facts>()?.compile_as_program()?);
 
